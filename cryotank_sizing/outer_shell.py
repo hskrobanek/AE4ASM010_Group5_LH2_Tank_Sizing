23da2f14e0b7f4999f98c040a932b2a108214224
Toine <tvanwassenaar@tudelft.nl>
2026-09-04 15:04:07 +0200

    continued vaccuum stuff

--- cryotank_sizing/outer_shell.py
+++ cryotank_sizing/outer_shell.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Inputs
 boil_off_rate = 0.1  # %/h, placeholder
 volume_LH2 = 10  # m3, placeholder
@@ -12,13 +13,19 @@ def heat_in_calculation(boil_off_rate, volume_LH2, density_LH2, latent_heat):
     return Qin
 
 
-def radius_outer(radius_inner, max_radius, thermal_conductivity_inner, ):
+def radius_outer(radius_inner, max_radius, Length, Qin, thermal_conductivity_inner, thermal_conductivity_air ):
     # assume 1 metal liner of 1 mm as inner shell
     # assume 1 layer of air unknown thickness
     # assume 1 layer of metal 1 mm as outer shell
     radius_inner_plus_thickness = radius_inner + 1
-    radius_left = max_radius
+    r_inner_ratio = radius_inner / radius_inner_plus_thickness 
+    radius_left = max_radius - radius_inner
     # for loop that goes from inner radius to maximum to see if Qin is low enough 
+    for i in range(radius_left):
+        r_vaccuum_ratio = radius_inner_plus_thickness + i -1
+        r_external = radius_inner_plus_thickness + i
+        Qin = (T_ext_env - T_int_surface)/(np.log())
+
 
 
     return outer_radius
